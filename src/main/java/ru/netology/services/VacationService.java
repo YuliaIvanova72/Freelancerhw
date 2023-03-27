c49a9a717e8f69f61a5feda68aab389c42c86ffb
Yulia Ivanova <ivanova.yuvl@gmail.com>
2023-03-27 12:30:39 +0500

    Initial commit

---
+++ src/main/java/ru/netology/services/VacationService.java
@@ -0,0 +1,22 @@
+package ru.netology.services;
+
+public class VacationService {
+
+
+    public int calculate(int income, int expenses, int threshold) {
+        int money = 0;
+        int count =0;
+
+
+        for (int month =0; month <12; month++) {
+            if (money >= threshold) {
+                money = money - expenses;
+                money = money / 3;
+                count++;
+            } else {
+                money = money + income - expenses;
+            }
+        }
+        return count;
+    }
+}
